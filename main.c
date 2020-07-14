0de8ff375ca7a640b31a22f43f02018ca96f8d16
daniel <vargass.daniel@hotmail.com>
2020-07-14 19:00:18 -0300

    fluxo da encriptação definido e funcionando

--- main.c
+++ main.c
@@ -6,13 +6,15 @@
 #include <stdbool.h>
 #include <string.h>
 #include <locale.h>
+#include <stdlib.h>
 
 #define TAM_BUFFER_READ_FILE 255
 #define TAM_ALFABETO 52
 
-void cifragem_monoalfabetica();
+int cifragem_monoalfabetica();
 void decifragem_monoalfabetica();
 bool read_dicionario(FILE *file_dictionary, char alfabeto[], char dicionario[]);
+int get_position(char vetor[], char c);
 
 int main(){
     setlocale(LC_ALL, "");
@@ -35,7 +37,10 @@ int main(){
 
                     switch (sub_opcao_menu) {
                         case 1:
-                            cifragem_monoalfabetica();
+                            if(cifragem_monoalfabetica() < 0) {
+                                printf("Erro ao abrir arquivo. (Certifique-se de digitar apenas "
+                                       "o nome e que seja um arquivo .txt)\n");
+                            }
                             break;
                         case 2:
                             break;
@@ -62,14 +67,73 @@ int main(){
     } while (opcao_menu_inicial != 0);
 }
 
-void cifragem_monoalfabetica(){
+/**
+ * Fluxo de execução para uma encriptação monoalfabetica,
+ * solicitando aberturas de arquivos necessarios e fazendo a encriptação
+ */
+int cifragem_monoalfabetica(){
     //TODO: implementar o algoritmo de cifragem
-    FILE *fdicionario = fopen("dicionario.txt","r");
-    char alfabeto[52];
-    char dicionario[52];
+    FILE *fdicionario;
+    FILE *fcipher;
+    FILE *fplaintext;
+    char alfabeto[TAM_ALFABETO];
+    char dicionario[TAM_ALFABETO];
+    char buffer[TAM_BUFFER_READ_FILE];
+
+    printf("Nome arquivo dicionario: ");
+    scanf("%s", &buffer);
+    fdicionario = fopen(strcat(buffer, ".txt"), "r");
+    if (fdicionario == NULL){
+        return -1;
+    }
+
+    printf("Nome arquivo plaintext: ");
+    scanf("%s", &buffer);
+    fplaintext = fopen(strcat(buffer, ".txt"), "r");
+    if (fplaintext == NULL){
+        return -1;
+    }
+
+    printf("Defina nome arquivo saida: ");
+    scanf("%s", &buffer);
+    fcipher = fopen(strcat(buffer, ".txt"), "w");
+    if (fcipher == NULL){
+        return -1;
+    }
 
     read_dicionario(fdicionario, alfabeto, dicionario);
+
+    while (!feof(fplaintext)){
+        fgets(buffer, TAM_BUFFER_READ_FILE, fplaintext);
+        int position;
+        for (int i = 0; i < TAM_BUFFER_READ_FILE || !feof(fplaintext); i++) {
+            if (buffer[i] != EOF && buffer[i] != '\0' && buffer[i] != '\n'){
+                if (buffer[i] != ' '){
+                    position = get_position(alfabeto, buffer[i]);
+                    if (position >= 0){
+                        putc(dicionario[position], fcipher);
+                    }
+                }
+            } else{
+                break;
+            }
+        }
+    }
 }
+
+/**
+ * Verifica a primira ocorrencia de um caractere dentro de um vetor
+ * @param vetor a ser analisado
+ * @param c caractere a ser encontrado
+ * @return a posição do caractere dentro de vetor, se não for encontrado retorna -1
+ */
+int get_position(char vetor[], char c){
+    for (int i = 0; i < TAM_ALFABETO; ++i) {
+        if (c == vetor[i]) return i;
+    }
+    return -1;
+}
+
 
 
 bool read_dicionario(FILE *file_dictionary, char alfabeto[], char dicionario[]){
